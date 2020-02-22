47466f1a8abcf3a6562d56ddfa7967b6cfd4fde7
675076143 <36965465+675076143@users.noreply.github.com>
2020-02-22 21:39:25 +0800

    fix bug

--- src/page/user/info/detail.vue
+++ src/page/user/info/detail.vue
@@ -1,13 +1,9 @@
 <template>
     <div>
-        <headerNav title="账号管理"/>
+        <headerNav title="Account"/>
         <van-cell-group>
-            <van-cell title="修改个人信息"  is-link to="/user/modify" />
-            <van-cell title="balance" :value="'￥'+parseFloat(user.balance).toFixed(2)" is-link to="/user/balance" />
-<!--            <van-cell title="修改登录密码"  is-link />-->
-<!--            <van-cell title="修改绑定手机"  is-link />-->
-<!--            <van-cell title="关联账号"  is-link />-->
-<!--            <van-cell title="切换账号"  is-link to="/login" />-->
+            <van-cell title="Profile"  is-link to="/user/modify" />
+            <van-cell title="Balance" :value="'￥'+parseFloat(user.balance).toFixed(2)" is-link to="/user/balance" />
         </van-cell-group>
     </div>
 </template>
